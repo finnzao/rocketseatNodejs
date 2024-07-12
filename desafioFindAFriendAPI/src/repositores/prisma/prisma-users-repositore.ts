68b50c1da7a4b720277aebdcf885781c61b236ef
Felipe <lipefelipe403@hotmail.com>
2024-07-11 22:28:06 -0300

    Eslint

---
+++ desafioFindAFriendAPI/src/repositores/prisma/prisma-users-repositore.ts
@@ -0,0 +1,49 @@
+import { prisma } from "@/lib/prisma";
+import { Prisma } from "@prisma/client"
+
+import { UsersRepository } from "../users-repository";
+
+export class PrismaUsersRepositore implements UsersRepository {
+
+    async findById(id: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                id,
+            },
+            
+        });
+
+        return user;
+    }
+
+    async findByEmail(email: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                email,
+            },
+        });
+
+        return user;
+    }
+
+
+
+    async findByName(name: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                name,
+            }
+        })
+        return user;
+    }
+
+
+
+    async create(data: Prisma.UserCreateInput) {
+        const user = await prisma.user.create({
+            data,
+        })
+
+        return user
+    }
+}
