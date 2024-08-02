fec7ab46728dd618c56e5e931e6239c187493f22
Felipe <lipefelipe403@hotmail.com>
2024-08-02 20:14:54 -0300

    factories FAker

--- 04-DDD e Primeiro framework/test/factories/make-question.ts
+++ 04-DDD e Primeiro framework/test/factories/make-question.ts
@@ -1,17 +1,18 @@
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Question, QuestionProps } from "@/domain/forum/enterprise/entities/question";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
-
+import { faker } from "@faker-js/faker"
 export function makeQuestion(
-    override: Partial<QuestionProps> = {}
+    override: Partial<QuestionProps> = {},
+    id?: UniqueEntityID
 ) {
     const question = Question.create({
-        title: "Example question",
+        title: faker.lorem.sentence(),
         slug: Slug.create("example-slug"),
         authorId: new UniqueEntityID(),
-        content: "Example content",
+        content: faker.lorem.text(),
         ...override
-    })
+    }, id,)
 
     return question
 }
