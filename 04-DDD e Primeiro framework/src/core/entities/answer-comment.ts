c58f23707b30e4424e084aef1e7433fdebf8da0f
Felipe <lipefelipe403@hotmail.com>
2024-08-06 13:56:10 -0300

    Class comment

--- 04-DDD e Primeiro framework/src/core/entities/answer-comment.ts
+++ 04-DDD e Primeiro framework/src/core/entities/answer-comment.ts
@@ -1,53 +1,26 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import { Comment, CommentProps } from './comment'
 
-export interface AnswerCommentProps {
-    authorId: UniqueEntityID
+export interface AnswerCommentProps extends CommentProps {
     answerId: UniqueEntityID
-    content: string
-    createdAt: Date
-    updatedAt?: Date
 }
-
-
-export class AnswerComment extends Entity<AnswerCommentProps> {
-    get authorId() {
+export class AnswerComment extends Comment<AnswerCommentProps> {
+    get answerId() {
         return this.props.authorId
-    }
-
-    get content() {
-        return this.props.content
-    }
-
-    get createdAt() {
-        return this.props.createdAt
-    }
-
-    get updatedat() {
-        return this.props.updatedAt
-    }
-
-    private touch() {
-        this.props.updatedAt = new Date()
-    }
-
-    set content(content: string) {
-        this.props.content = content
-        this.touch()
     }
 
     static create(
         props: Optional<AnswerCommentProps, 'createdAt'>,
         id?: UniqueEntityID,
     ) {
-        const answerComment = new AnswerComment({
-            ...props,
-            createdAt: props.createdAt ?? new Date()
-        },
+        const answerComment = new AnswerComment(
+            {
+                ...props,
+                createdAt: props.createdAt ?? new Date(),
+            },
             id,
         )
-
         return answerComment
     }
 }
