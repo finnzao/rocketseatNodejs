e099088a745888a655e6a19b8562b02346199823
Felipe <lipefelipe403@hotmail.com>
2024-08-08 12:34:09 -0300

    Fetch Question Comment Use Case

--- 04-DDD e Primeiro framework/test/repositories/in-memory-question-comments-repository.ts.ts
+++ 04-DDD e Primeiro framework/test/repositories/in-memory-question-comments-repository.ts.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/domain/forum/application/repositories/pagination-params'
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 export class InMemoryQuestionCommentsRepository
@@ -24,5 +25,12 @@ export class InMemoryQuestionCommentsRepository
         )
 
         this.items.splice(itemIndex, 1)
+    }
+    async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+        const questionComments = this.items
+            .filter((item) => item.questionId.toString() === questionId)
+            .slice((page - 1) * 20, page * 20)
+
+        return questionComments
     }
 }
