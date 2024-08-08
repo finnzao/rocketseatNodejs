7b569159aff72753c814449f8b3c199ec2dfce89
Felipe <lipefelipe403@hotmail.com>
2024-08-08 13:39:16 -0300

    Fetch Answer Comment

--- 04-DDD e Primeiro framework/src/domain/forum/application/repositories/answer-comments-repository.ts
+++ 04-DDD e Primeiro framework/src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,7 +1,12 @@
-import { AnswerComment } from '../../enterprise/entities/answer-comment'
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
 export interface AnswerCommentsRepository {
-    create(AnswerComment: AnswerComment): Promise<void>
     findById(id: string): Promise<AnswerComment | null>
-    delete(AnswerComment: AnswerComment): Promise<void>
+    findManyByAnswerId(
+        answerId: string,
+        params: PaginationParams,
+    ): Promise<AnswerComment[]>
+    create(answerComment: AnswerComment): Promise<void>
+    delete(answerComment: AnswerComment): Promise<void>
 }
